44ffa33d3a7fff70a794d6dbfd938b5c178f69d2
Marcus Veres <marcus.veres@gmail.com>
2014-08-14 16:23:10 -0400

    created a loop + data model for generating menu items

--- menu.js
+++ menu.js
@@ -1,9 +1,25 @@
 // -------------------------------------------------------------------
 // Context Menu Code
 
+console.log( "version 0.0.0.018" );
+
 
 // -------------------------------------------------------------------
-// Application Functions
+// Application Functions 
+
+    var functions = 
+    {
+        // perform a search query on a website
+        generic_search : function()
+        {
+            var _args = arguments[0];
+            var info = _args['info'];
+            var query = _args['caller']['query_root'] || 'http://google.com/search?q=';
+            query += info.selectionText;
+            console.log("query is:" , query);
+            chrome.tabs.create({ "url" : query });
+        }
+    }
 
     // General Functions
 
@@ -16,24 +32,6 @@
 
     // Selection Functions
 
-    function search_google(info)
-    {
-        var query = "http://google.com/search?q=" + info.selectionText;
-        chrome.tabs.create({ "url" : query });
-    }
-
-    function search_google_maps(info)
-    {
-        var query = "http://maps.google.co.m/maps?q=" + info.selectionText;
-        chrome.tabs.create({ url: query })
-    }
-
-    function search_stack_overflow(info)
-    {
-        var query = "http://stackoverflow.com/search?q=" + info.selectionText;
-        chrome.tabs.create({ "url" : query });
-    }
-
     // Link Functions
 
     function open_link(info)
@@ -45,6 +43,42 @@
             "url" : url
         });
     }
+
+
+// -------------------------------------------------------------------
+// Arrays / Data
+
+var data_object = 
+{
+    "menu_items" : 
+    [
+        {   
+            "name" : "google",
+            "query_root" : "http://google.com/search?q=",
+            "title" : "Search on Google",
+            "function_name" : "generic_search",
+            "contexts" : ["selection"],
+            "parent_id" : "select_parent"
+        },
+        {   
+            "name" : "google_maps",
+            "query_root" : "http://maps.google.com/maps?q=",
+            "title" : "Search on Google Maps",
+            "function_name" : "generic_search",
+            "contexts" : ["selection"],
+            "parent_id" : "select_parent"
+        },
+        {   
+            "name" : "stack_overflow",
+            "query_root" : "http://stackoverflow.com/search?q=",
+            "title" : "Search on Stack Overflow",
+            "function_name" : "generic_search",
+            "contexts" : ["selection"],
+            "parent_id" : "select_parent"
+        }
+    ]
+
+} // END - data object
 
 
 // -------------------------------------------------------------------
@@ -68,6 +102,46 @@
     });
 
 
+// Loop through the children
+// iterate through the data object and generate a function for each one
+for( var i = 0 , length = data_object.menu_items.length ; i < length ; i++ )
+{
+    (function()
+    {
+        var index = i;
+        var args = arguments[0];       
+ 
+        var root = data_object.menu_items[i];
+        // console.log( root.name , " : " , root.query_root , " : " , root.title );
+        //console.log( "i is ", i );
+        //console.log( "arguments be" , arguments );
+
+        chrome.contextMenus.create({
+            "title": root.title,
+            "parentId" : window[root.parent_id], // TODO: avoid polluting the global namespace + update this
+            "contexts" : root.contexts,
+            "onclick" : function( info , tab ){
+                // get the function from the set of functions
+                // feed it an object as an argument 
+                functions[root.function_name]({ 
+                    "info" : info , "caller" : root 
+                });
+            }
+            // This is not dynamic ; would have to create a separate loop for all functionality
+            /* 
+            "onclick": function(info){
+                var query = root.query_root + info.selectionText;
+                chrome.tabs.create({ "url" : query });
+            }
+            */
+
+        });   
+
+    }).apply( this , [{ "something" : "wong" , "anything" : "white" , "i_value" : i  }]);
+
+} 
+
+
 // -------------------------------------------------------------------
 // Define the Children
 
@@ -80,27 +154,7 @@
     });
 
     // Selection Children
-
-    var child_query_google = chrome.contextMenus.create({
-        "title": "Find on Google",
-        "parentId" : select_parent,
-        "contexts": ["selection"],
-        "onclick": search_google
-    });
-
-    var child_query_google_maps = chrome.contextMenus.create({
-        "title": "Find on Google Maps",
-        "parentId": select_parent,
-        "contexts": ["selection"],      // lets chrome know we're using the selection
-        "onclick": search_google_maps   // function to call
-    });
-
-    var child_query_stack_overflow = chorome.contextMenus.create({
-        "title": "Stack Ovelflow",
-        "parentId" : select_parent,
-        "contexts" : ["selection"],
-        "onclick" : search_stack_overflow
-    });
+    // ( see above )
 
     // Link Children
     var child_link_open = chrome.contextMenus.create({
